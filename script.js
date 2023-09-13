a14b61e6b70ec10eef7acaac54af9311145573f8
Márcio Diniz <marcio.sccp10@gmail.com>
2023-09-13 13:54:06 -0300

    Updates

--- script.js
+++ script.js
@@ -22,9 +22,17 @@ document.addEventListener('keydown', (event) => {
 
     if (marginLeft > -200 && event.key === 'a' || event.key === 'A') {
         marginLeft -= 200;
+        car.classList.add('car-left');
+        setTimeout(() => {
+            car.classList.remove('car-left');
+        }, 500);
     }
     else if (marginLeft < 200 && event.key === 'd' || event.key === 'D') {
         marginLeft += 200;
+        car.classList.add('car-right');
+        setTimeout(() => {
+            car.classList.remove('car-right');
+        }, 500);
     }
 
     switch (marginLeft) {
