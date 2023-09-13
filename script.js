e2d837a74833edfd12bebc62eb8f174c23c2bab6
Márcio Diniz <marcio.sccp10@gmail.com>
2023-09-13 12:41:57 -0300

    Updates

---
+++ script.js
@@ -0,0 +1,53 @@
+const interface = document.querySelector('.interface');
+
+const lSide = document.createElement('div');
+lSide.classList.add('l-side');
+interface.appendChild(lSide);
+
+const car = document.createElement('div');
+car.classList.add('car');
+interface.appendChild(car);
+
+const rSide = document.createElement('div');
+rSide.classList.add('r-side');
+interface.appendChild(rSide);
+
+
+// Controles carro
+
+const marginLeftMin = -200;
+const marginLeftMax = 200;
+let marginLeft = 0;
+
+document.addEventListener('keydown', (event) => {
+    if (marginLeft > -200 && event.key === 'a' || event.key === 'A') {
+        marginLeft -= 200;
+    }
+    else if (marginLeft < 200 && event.key === 'd' || event.key === 'D') {
+        marginLeft += 200;
+    }
+
+
+
+    switch (marginLeft) {
+        case 0:
+            car.style.marginLeft = 0;
+            break;
+    
+        case -200:
+            car.style.marginLeft = '-200px';
+            break;
+    
+        case 200:
+            car.style.marginLeft = '200px';
+            break;
+    
+        default:
+            break;
+    }
+
+    console.log(marginLeft);
+      
+});
+
+
